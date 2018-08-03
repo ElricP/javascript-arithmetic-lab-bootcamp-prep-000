df80f5fab08b350dae902150ab083f5b39bcafb3
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-03 20:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function add(a, b){
   return a+b;
 }
+
+function substract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n){
+  n++;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
